25f866b252d4015cf25897abe3910d7e3bdb6274
AranxxaLL <arlolocatt@gmail.com>
2022-12-21 00:21:26 -0600

    js

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let ayuda=prompt("¿Cual es tu nombre?" );
+const frase= prompt("¿Que ayuda necesitas?");
+const info=alert('Hola' ${ayuda},'te ayudaremos con' ${frase});
+
+const edad=parseInt(prompt("Edad por favor"));
+for (var i=0; i<10; i++){
+    n +=i;
+    promedio = n /i;
+    promedio(promedio);
+}
+const edadfinal=alert('Edad promedio de nuestros clientes' ${promedio});
+
+const horario = Boolean(true);
+if (horario == true){
+    alert("Terapia en consultori")
+}else{
+    alert("Terapia en linea");
+}
